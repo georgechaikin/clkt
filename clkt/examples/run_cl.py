2c459e02614ae75cc057454b72bdd11f60acd3a6
George Chaikin <chaykin.georgiy217@gmail.com>
2024-03-31 21:41:07 +0300

    Config params support and type hints added.

--- clkt/examples/run_cl.py
+++ clkt/examples/run_cl.py
@@ -6,20 +6,22 @@ import os
 import random
 from functools import partial
 from pathlib import Path
-from typing import Union
+from typing import Union, Iterable, Dict
 
 import click
 import numpy as np
 import torch
 import wandb
 from avalanche.benchmarks import benchmark_from_datasets, task_incremental_benchmark, AvalancheDataset
+from avalanche.benchmarks.scenarios.generic_scenario import TCLStream
 from avalanche.training import EWC, Naive, Cumulative, Replay, FromScratchTraining
+from avalanche.training.templates import SupervisedTemplate
 from torch.nn.functional import binary_cross_entropy
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
 
-from clkt.config import ConfigFile
+from clkt.config import ConfigFile, YAMLType
 from clkt.datasets.assist2009 import ASSIST2009
 from clkt.datasets.utils import collate_fn
 from clkt.datasets.utils import get_datasets, random_datasets_split
@@ -28,16 +30,18 @@ from clkt.models.sakt import SAKT
 from clkt.models.utils import get_eval_metrics
 
 
-def run_experiment(cl_strategy, model, train_stream, test_loaders, test_loader, config_args):
-    """Runs the experiment for defined CL strategy.
+def run_experiment(cl_strategy: SupervisedTemplate, model: torch.Module, train_stream: TCLStream,
+                   test_loaders: Iterable[DataLoader], test_loader: DataLoader,
+                   config_args: Dict[str, YAMLType]) -> None:
+    """Runs the experiment for the specific CL strategy.
 
     Args:
-        cl_strategy:
-        model:
-        train_stream:
-        test_loaders:
-        test_loader:
-        config_args:
+        cl_strategy: Continual Learning strategy
+        model: Knowledge Tracing model for ASSISTMENT2009.
+        train_stream: Train stream from benchmark object.
+        test_loaders: List of test loaders.
+        test_loader: Test loader for the overall ASSISTMENT2009 dataset.
+        config_args: Config parameters for wandb logging.
 
     """
 
@@ -86,10 +90,13 @@ def manual_seed(seed: int) -> None:
               help='Config file with parameters.')
 def run_experiments(data_path: Union[str, os.PathLike], config=Union[str, os.PathLike]):
     """Runs the Continual learning experiment with some Avalanche strategies and knowledge tracing model."""
-    config_args = ConfigFile(config).config
-    device = torch.device('cpu')
 
-    seed = 0
+    # Define config parameters.
+    config_args = ConfigFile(config)
+
+    # Reproducibility and device.
+    device = torch.device(config_args.device)
+    seed = config_args.seed
     manual_seed(seed)
     g = torch.Generator(device=device)
     g.manual_seed(seed)
@@ -98,12 +105,13 @@ def run_experiments(data_path: Union[str, os.PathLike], config=Union[str, os.Pat
     dataset_path = Path(data_path)
     dataset = ASSIST2009(100, dataset_path)
 
-    train_ratio = 0.1
+    train_ratio = config_args.train_ratio
+    split_ratio = config_args.split_ratio
     train_size = int(len(dataset) * train_ratio)
     test_size = len(dataset) - train_size
     train_dataset, test_dataset = random_split(dataset, [train_size, test_size], generator=g)
-    train_datasets = get_datasets(train_dataset, split_ratio=0.1, generator=g)
-    train_datasets, test_datasets = random_datasets_split(train_datasets, 0.9, generator=g)
+    train_datasets = get_datasets(train_dataset, split_ratio=split_ratio, generator=g)
+    train_datasets, test_datasets = random_datasets_split(train_datasets, train_ratio, generator=g)
     train_datasets = [AvalancheDataset(ds, collate_fn=collate_fn) for ds in train_datasets]
     bm = benchmark_from_datasets(train=train_datasets)
     bm = task_incremental_benchmark(bm)
@@ -111,30 +119,28 @@ def run_experiments(data_path: Union[str, os.PathLike], config=Union[str, os.Pat
 
     # Set test loaders
     test_loader = DataLoader(test_dataset, batch_size=len(test_dataset),
-                             collate_fn=partial(collate_fn, return_task=False),
-                             generator=g)
+                             collate_fn=partial(collate_fn, return_task=False), generator=g)
     test_loaders = []
     for ds in test_datasets:
         test_loader = DataLoader(ds, batch_size=len(ds), collate_fn=partial(collate_fn, return_task=False), generator=g)
         test_loaders.append(test_loader)
 
     # Set model, criterion and optimizer.
-    model = SAKT(564, n=100, d=100, num_attn_heads=5, dropout=0.2)
-    # model.to(device)
+    batch_size = config_args.batch_size
+    epochs = config_args.epochs
+    seq_len = config_args.seq_len
+    model = SAKT(564, n=seq_len, d=seq_len, num_attn_heads=5, dropout=0.2)
     optimizer = Adam(model.parameters(), lr=0.001)
     criterion = binary_cross_entropy
 
-    from_scratch = FromScratchTraining(model, optimizer, criterion, train_mb_size=config_args['batch_size'],
-                                       train_epochs=config_args['epochs'], device=device)
+    # CL strategies.
+    from_scratch = FromScratchTraining(model, optimizer, criterion, train_mb_size=batch_size, train_epochs=epochs,
+                                       device=device)
 
-    naive = Naive(model, optimizer, criterion, train_mb_size=config_args['batch_size'],
-                  train_epochs=config_args['epochs'],
-                  eval_mb_size=32, device=device)
+    naive = Naive(model, optimizer, criterion, train_mb_size=batch_size, train_epochs=epochs, eval_mb_size=32,
+                  device=device)
 
-    config_args['ewc_lambda'] = 0.1
-    ewc = EWC(model, optimizer, criterion, ewc_lambda=config_args['ewc_lambda'],
-              train_mb_size=config_args['batch_size'],
-              train_epochs=config_args['epochs'],
+    ewc = EWC(model, optimizer, criterion, ewc_lambda=config.ewc_lambda, train_mb_size=batch_size, train_epochs=epochs,
               eval_mb_size=32, device=device)
 
     cumulative = Cumulative(model, optimizer, criterion, train_mb_size=config_args['batch_size'],
@@ -146,5 +152,6 @@ def run_experiments(data_path: Union[str, os.PathLike], config=Union[str, os.Pat
 
     strategies = [from_scratch, naive, ewc, replay, cumulative]
 
+    # Run experiments
     for strategy in strategies:
-        run_experiment(strategy, model, train_stream, test_loaders, test_loader, config_args)
+        run_experiment(strategy, model, train_stream, test_loaders, test_loader, config_args.config)
